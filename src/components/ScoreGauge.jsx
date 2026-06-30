226024192725bc03585535b27a203e5967c4e121
Tejputty123 <tejpraneet123@gmail.com>
2026-06-30 09:53:30 +0530

    Fix ATS gauge centering

--- src/components/ScoreGauge.jsx
+++ src/components/ScoreGauge.jsx
@@ -5,23 +5,23 @@ export default function ScoreGauge({ score }) {
   const data = [{ value: score, fill: color }];
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="relative" style={{ width: 200, height: 140 }}>
-        <RadialBarChart 
-          width={200} 
-          height={200} 
-          innerRadius="65%" 
+    <div className="flex flex-col items-center justify-center" style={{ width: 220, height: 220 }}>
+      <div className="relative" style={{ width: 220, height: 220 }}>
+        <RadialBarChart
+          width={220}
+          height={220}
+          innerRadius="70%"
           outerRadius="100%"
-          data={data} 
-          startAngle={180} 
-          endAngle={0}
+          data={data}
+          startAngle={90}
+          endAngle={-270}
           cx="50%"
-          cy="100%"
+          cy="50%"
         >
           <PolarAngleAxis type="number" domain={[0, 100]} angleAxisId={0} tick={false} />
           <RadialBar background dataKey="value" cornerRadius={10} angleAxisId={0} />
         </RadialBarChart>
-        <div className="absolute inset-0 flex flex-col items-center justify-end pb-2">
+        <div className="absolute inset-0 flex flex-col items-center justify-center pointer-events-none">
           <p className="text-4xl font-bold" style={{ color }}>{score}</p>
           <p className="text-sm text-gray-500">ATS Score</p>
         </div>
